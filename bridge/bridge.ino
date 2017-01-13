cea3130ffb54743a54658d7fa8b591c8abf46e94
Brendon Earl <bearl@uvic.ca>
2017-01-13 10:28:19 -0700

    init w/ basic state management code

---
+++ bridge/bridge.ino
@@ -0,0 +1,53 @@
+#define stdio.h
+#define stdlib.h
+
+const int PROXN = 1;    // north prox sensor
+const int PROXS = 2;    // south prox sensor
+const int MOTN = 3;     // north motor sensor
+const int MOTS = 4;     // south motor sensor
+
+typedev enum Bool {
+  FALSE,
+  TRUE
+}
+
+typedef enum mode {
+  ERROR,
+  OK,
+  LIFTING,
+  LIFTED,
+  LOWERING,
+  QUAKE
+} Mode;
+
+typedef struct State {
+  Mode mode;
+
+};
+
+State state;
+
+void setup() {
+  state.mode = OK;
+}
+
+void loop() {
+  switch (state.mode) {
+    case OK:
+      break;
+    case LIFTING:
+      break;
+    case LIFTED:
+      break;
+    case LOWERING:
+      break;
+    case QUAKE:
+      break;
+    case ERROR:
+      break;
+    default:
+      Serial.println("Fallen into default state, setting to ERROR");
+      state.mode = ERROR;
+      break;
+  }
+}
