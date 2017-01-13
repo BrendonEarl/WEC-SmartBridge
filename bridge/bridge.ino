d2dc24d51b13f753bfc4bbb0b5e877b3098107c9
Brendon Earl <bearl@uvic.ca>
2017-01-13 13:21:28 -0700

    Add helper functions

--- bridge/bridge.ino
+++ bridge/bridge.ino
@@ -5,15 +5,12 @@ const int PROXN = 1;    // north prox sensor
 const int PROXS = 2;    // south prox sensor
 const int MOTN = 3;     // north motor sensor
 const int MOTS = 4;     // south motor sensor
-
-typedef enum Bool {
-  FALSE,
-  TRUE
-};
+const int MOTCONT = 5;  // motor control
 
 typedef enum mode {
   ERROR,
   OK,
+  LIFTWAIT,
   LIFTING,
   LIFTED,
   LOWERING,
@@ -27,6 +24,30 @@ typedef struct State {
 
 State state;
 
+boolean shouldRaise() {
+  return true;
+}
+
+boolean safeToRaise() {
+  return true;
+}
+
+boolean isRaised() {
+  return true;
+}
+
+boolean shouldLower() {
+  return true;
+}
+
+boolean safeToLower() {
+  return true;
+}
+
+boolean isLowered() {
+  return true;
+}
+
 void setup() {
   state.mode = OK;
 }
@@ -34,12 +55,19 @@ void setup() {
 void loop() {
   switch (state.mode) {
     case OK:
+      if (shouldRaise()) state.mode = LIFTWAIT;
+      break;
+    case LIFTWAIT:
+      if (safeToRaise()) state.mode = LIFTING;
       break;
     case LIFTING:
+      if (isRaised()) state.mode = LIFTED;
       break;
     case LIFTED:
+      if (shouldLower() && safeToLower()) state.mode = LOWERING;
       break;
     case LOWERING:
+      if (isLowered()) state.mode = OK;
       break;
     case QUAKE:
       break;
@@ -49,5 +77,7 @@ void loop() {
       Serial.println("Fallen into default state, setting to ERROR");
       state.mode = ERROR;
       break;
+
+    // check for button pushes
   }
 }
